c4fa66f717468bc85bbddfcee2494a534a9b49c9
Jonathan Miller <jmillerneteng@gmail.com>
2018-07-25 16:30:16 -0500

    ex29

---
+++ ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("too many cats")
+
+if people > cats:
+    print("good, i hate cats")
+
+if people < dogs:
+    print("dog drool, kk happy")
+
+if people > dogs:
+    print("less drool, kk still happy")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater or equal to dogs")
+
+if people <= dogs:
+    print("people are less than or equal to dogs")
+
+if people == dogs:
+    print("people are dogs")
