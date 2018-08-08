46bc28af2f1b322f2b0bb59eae0d166f4ca410f7
JM <jmillerneteng@gmail.com>
2018-08-08 09:55:04 -0500

    mod ex32

--- ex32.py
+++ ex32.py
@@ -3,8 +3,8 @@ fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
 #the first kind of for-loop goes through a list
-for numberz in the_count:
-    print(f"This is count{numberz}")
+for number in the_count:
+    print(f"This is count: {number}")
 
 # same as above
 for fruit_list in fruits:
@@ -32,3 +32,12 @@ for i in elements:
 #print the entire list in elements
 print(elements)
 
+
+for j in range(10,50):
+    print(f"This is number i created {j}")
+    elements.append(j)
+
+for j in elements:
+    print(f"new func: {j}")
+
+print (elements)
